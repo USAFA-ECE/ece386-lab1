aec00c7edf286a360f229012ccee284dbdcb5949
byarbrough <6315292+byarbrough@users.noreply.github.com>
2025-01-31 11:48:20 -0700

    Provide more structure for server

--- server/digits.py
+++ server/digits.py
@@ -1,6 +1,9 @@
 """
-TODO: Insert what this program does here. Should start with
-digits is a FastAPI app that...
+digits is a FastAPI app that hosts a Keras classification model for handwritten digits.
+
+Usage: fastapi run digits.py
+
+Will auto-create OpenAPI docs at /redoc
 
 This file should be compliant with Pyright.
 The tensorflow import is ignored with # type: ignore[import]
@@ -10,20 +13,39 @@ because tensorflow doesn't support type hints appropriately.
 from tensorflow.keras.saving import load_model  # type: ignore[import]
 from PIL import Image
 from io import BytesIO
+import numpy as np
+from fastapi import FastAPI, File
+from typing import Annotated
+
 
 model_path: str = "digits.keras"
-# TODO: Open saved Keras model as global variable. NO TYPE HINT REQUIRED!
+# TODO: Use load_model that's imported above to open saved Keras model as global variable. NO TYPE HINT REQUIRED!
 
-# TODO: Create FastAPI App as global variable
+# Create the FastAPI app
+app: FastAPI = FastAPI()
 
 
 def image_to_np(image_bytes: bytes) -> np.ndarray:
-    """Convert image to proper numpy array"""
+    """image_to_np converts an image as bytes to proper numpy array via Pillow.
+    It also reshapes the image to ensure that it's the shape the Keras model expects."""
     # First must use pillow to process bytes
     img = Image.open(BytesIO(image_bytes))
     # TODO: convert image to grayscale and resize
     # TODO: convert image to numpy array of shape model expects
-    return None
+    return None  # TODO: replace with np array so Pyright stops complaining
 
 
-# TODO: Define predict POST function
+@app.post("/predict")
+def get_prediction(image: Annotated[bytes, File()]) -> dict[str, int]:
+    """get_prediction listens for a POST request to /predict and
+    returns the results of the AI classification of that image.
+    The POST content must contain { "image": bytes[] } or the function will return 422 error.
+    """
+
+    # Convert the bytes image into a format our model can use
+    processed_image = image_to_np(image)
+
+    # TODO: Run inference on the processed image
+
+    # TODO: Replace this demo dict with the result of the prediction
+    return {"file_size": len(image)}
